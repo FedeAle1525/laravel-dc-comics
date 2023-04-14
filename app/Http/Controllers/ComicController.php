966f6c1788d71ab7a08b05070a948b5764890ad5
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-14 18:27:39 +0200

    PHP add method show' in ComicController for ... ... CRUD Operation "Read/Show"

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -21,4 +21,18 @@ class ComicController extends Controller
         // Nome della Vista dentro la cartella Comics, da non confondere con la Rotta
         return view('comics.index', $data);
     }
+
+    // 1 - CRUD ---> Read ---> Show
+
+    // Passando Istanza Comic al metodo e inviadola alla Vista, un automatismo di Laravel fara' la ricerca automatica per $id
+    public function show(Comic $comic)
+    {
+        // Il metodo 'find' andrà a generare una Query che cerchera nella Tabella Books la riga con $id corrispondente
+        // $book = Book::find($id);
+
+        // Il metodo 'findOrFail' si comporta allo stesso modo del 'find', ma in caso di fallimento genera PAGINA 404
+        // $book = Book::findOrFail($id);
+
+        return view('comics.show', compact('comic'));
+    }
 }
