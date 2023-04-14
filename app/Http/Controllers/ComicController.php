13a093bbef0985c491870ac66bc8641d89365850
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-14 17:07:50 +0200

    PHP add method 'index' in ComicController for ... ... CRUD Operation "Read/Index"

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    //
+    // 1 - CRUD ---> Read ---> Index
+    public function index()
+    {
+
+        $comics = Comic::all();
+
+        // dd($comics);
+
+        // "Compact" crea un Array associativo con la CHIAVE 'comics' a cui associa come VALORE l'omonima variabile $comics
+        $data = compact('comics');
+
+        // Nome della Vista dentro la cartella Comics, da non confondere con la Rotta
+        return view('comics.index', $data);
+    }
 }
