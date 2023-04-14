6af6b1b0260750c9d32e9c717cbd893de6b311ab
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-14 19:56:45 +0200

    FEAT create template for View "Show" for ... ... CRUD Operation "Create"

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<main>
+  <div class="container">
+    <form action="">
+      FORM Nuovo Comic
+    </form>
+  </div>
+</main>
+
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,7 @@
       <li><a href="{{ route('home') }}">Home</a></li>
       <!-- 1 - CRUD / Read / Index -->
       <li><a href="{{ route('comics.index') }}">Comics</a></li>
+      <li><a href="{{ route('comics.create') }}">Create</a></li>
     </ul>
   </nav>
 </header>
