f06514829e8a4c3458c71c01b2d0229b270c7c8d
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-19 17:01:26 +0200

    EDIT add 'messagge error' for each input ... ... in form (Bonus 2)

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -18,32 +18,45 @@
 
       <!-- I Name degli Input devono coincidere con il nome delle Colonne della Tabella -->
       <div class="mb-3">
-        <label class="form-label">Titolo</label>
-        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+        <label for="title" class="form-label">Titolo</label>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
       </div>
+      <!-- Gestione degli Errori per ogni Input con Bootstrap -->
+      @error('title')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label d-block">Descrizione</label>
         <!-- In caso di errore ripopolo l'input con il vecchio valore inviato (old), per evitare il più possibile il medesimo errore -->
-        <textarea name="description" cols="100" rows="10">
+        <textarea class="form-control @error('description') is-invalid @enderror" name="description" cols="100" rows="10">
 
         {{ old('description') }}
 
         </textarea>
       </div>
+      @error('description')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Link Immagine</label>
-        <input type="text" class="form-control" name="thumb" value="{{ old('thumb') }}">
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" value="{{ old('thumb') }}">
       </div>
+      @error('thumb')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Serie</label>
-        <input type="text" class="form-control" name="series" value="{{ old('series') }}">
+        <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" value="{{ old('series') }}">
       </div>
+      @error('series')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
-        <label class="form-label">Tipologia</label>
+        <label class="form-label @error('type') is-invalid @enderror">Tipologia</label>
         <select type="text" class="form-control" name="type">
           <option>Seleziona Tipologia</option>
           <!-- Gestione Valori 'Old' per <Select> -->
@@ -51,19 +64,28 @@
           <option @selected( old('type')=='graphic novel' ) value="graphic novel">Graphic Novel</option>
         </select>
       </div>
+      @error('type')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Prezzo</label>
         <div class="input-group">
-          <input type="text" class="form-control" name="price" value="{{ old('price') }}">
+          <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('price') }}">
           <span class="input-group-text">€</span>
         </div>
       </div>
+      @error('price')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Data D'Uscita</label>
-        <input type="date" class="form-control" name="sale_date" value="{{ old('sale_date') }}">
+        <input type="date" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date" value="{{ old('sale_date') }}">
       </div>
+      @error('sale_date')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <button type="submit" class="btn btn-primary">Aggiungi</button>
@@ -72,7 +94,7 @@
     </form>
 
     <!-- Stampa degli Errori generati dalla Validazione della Richiesta (validi per UNA SESSIONE) -->
-    @if ($errors->any())
+    <!-- @if ($errors->any())
     <div class="alert alert-danger">
       <ul>
         @foreach ($errors->all() as $error)
@@ -80,7 +102,7 @@
         @endforeach
       </ul>
     </div>
-    @endif
+    @endif -->
 
   </div>
 </main>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -21,50 +21,72 @@
       <!-- I Value degli Input devono coincidono con il contenuto delle Colonne della Tabella, recuperati dal DB -->
       <div class="mb-3">
         <label class="form-label">Titolo</label>
-        <input type="text" class="form-control" name="title" value="{{ old('title', $comic->title) }}">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title', $comic->title) }}">
       </div>
+      <!-- Gestione degli Errori per ogni Input con Bootstrap -->
+      @error('title')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label d-block">Descrizione</label>
 
         <!-- In caso di errore ripopolo l'input con il vecchio valore inviato (old), per evitare il più possibile il medesimo errore -->
-        <textarea name="description" cols="100" rows="10">
+        <textarea class="form-control @error('description') is-invalid @enderror" name="description" cols="100" rows="10">
         {{ old('description', $comic->description)  }}
         </textarea>
       </div>
+      @error('description')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Link Immagine</label>
-        <input type="text" class="form-control" name="thumb" value="{{ old('thumb', $comic->thumb) }}">
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" value="{{ old('thumb', $comic->thumb) }}">
       </div>
+      @error('thumb')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Serie</label>
-        <input type="text" class="form-control" name="series" value="{{ old('series', $comic->series) }}">
+        <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" value="{{ old('series', $comic->series) }}">
       </div>
+      @error('series')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Tipologia</label>
-        <select type="text" class="form-control" name="type">
+        <select type="text" class="form-control @error('type') is-invalid @enderror" name="type">
           <option>Seleziona Tipologia</option>
           <!-- Gestione Valori 'Old' per <Select> -->
           <option @selected( old('type', $comic->type) == 'comic book' ) value="comic book">Comic Book</option>
           <option @selected( old('type', $comic->type) == 'graphic novel' ) value="graphic novel">Graphic Novel</option>
         </select>
       </div>
+      @error('type')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Prezzo</label>
         <div class="input-group">
-          <input type="text" class="form-control" name="price" value="{{ old('price', $comic->price) }}">
+          <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('price', $comic->price) }}">
           <span class="input-group-text">€</span>
         </div>
       </div>
+      @error('price')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <label class="form-label">Data D'Uscita</label>
-        <input type="date" class="form-control" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+        <input type="date" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
       </div>
+      @error('sale_date')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="mb-3">
         <button type="submit" class="btn btn-danger">Salva</button>
@@ -73,7 +95,7 @@
     </form>
 
     <!-- Stampa degli Errori generati dalla Validazione della Richiesta (validi per UNA SESSIONE) -->
-    @if ($errors->any())
+    <!-- @if ($errors->any())
     <div class="alert alert-danger">
       <ul>
         @foreach ($errors->all() as $error)
@@ -81,7 +103,7 @@
         @endforeach
       </ul>
     </div>
-    @endif
+    @endif -->
 
   </div>
 </main>
